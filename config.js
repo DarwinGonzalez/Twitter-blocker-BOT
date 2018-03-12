2295607d19d7eb6de6519f97a8cc09c5d55090a5
Darwin Gonzalez Suarez <alu0100812794@ull.edu.es>
2018-03-12 13:04:08 +0000

    Añadiendo primeros archivos código fuente del bot

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'd7OcjmqEcmcNGsAXO7YBTEwVY',  
+  consumer_secret: '0zrPsch2RxxqdIU8f7nChbZPlUeHoD5kF26m7TcpKTIcfxgTR2',
+  access_token: '457357405-vJczQg4oZ52DaI3bhtX09iN9TpAwdTvVnOBr4nMp',  
+  access_token_secret: 'mOqJX7K87yloeqbpKRyyX0ny2xUza38YYacdCnczsx5Dg'
+}
