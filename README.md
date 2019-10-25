6b9aa5695e5a4935c3e0a51d1d25fd3bb2c6e9d8
DarwinGonzalez <alu0100812794@ull.edu.es>
2019-10-25 10:00:25 +0200

    Blocking from twitter with the Twitter API

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Testing Twitter Bot
-Testeando un BOT de Twitter en Nodejs, que hace un RT dado un intervalo de tiempo y unas palabras clave dadas.
+# Testing Twitter bot
+Test twitter API blocking users from your twitter that have "The flag of Spain icon" in their username. You can fully customize who you want to block. **DO NOT stand b*******s, block them!**
+
+*PD*: in order to use this app you will have to configure the *./config* file parameters with your twitter API credentials.
+
+
+
+
 
--- bot.js
+++ bot.js
@@ -1,30 +1,16 @@
+'use strict';
 
-var twit = require('twit');
-var config = require('./config');
+const twit = require('twit');
+const config = require('./config');
 
-var Twitter = new twit(config);
+const Twitter = new twit(config);
 
-var retweet = function() {
+const findSpaniards = function() {
     
-    var params = {
-        q: '#SRV, #Guitar',  // REQUIRED
-        result_type: 'recent',
-        lang: 'en'
-    }
-    
-    Twitter.get('search/tweets', params, function(err, data) {
+    Twitter.get('friends/list', function(err, data) {
         if (!err) {
-            var retweetId = data.statuses[0].id_str;
-            Twitter.post('statuses/retweet/:id', {
-                id: retweetId
-            }, function(err, response) {
-                if (response) {
-                    console.log('Retweeted!!!');
-                }
-                if (err) {
-                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
-                }
-            });
+            const result = data.users.filter(data => data.name.includes('🇪🇸'));
+            blockSpaniards(result);
         }
         else {
           console.log('Something went wrong while SEARCHING...');
@@ -32,6 +18,18 @@ var retweet = function() {
     });
 }
 
-retweet();
-setInterval(retweet, 3000000);
+const blockSpaniards =  function(result) {
+    result.forEach(element => {
+        console.log(element.name);
+        Twitter.post('blocks/create', {screen_name: element.screen_name}, function(err, data) {
+            if(!err) {
+                console.log(`User ${element.screen_name} have been blocked!`);
+            } else {
+                console.log('Something went wrong while BLOCKING...');
+            }
+        })
+    });
+    
+}
 
+findSpaniards();
